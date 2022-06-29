ff5a02f6a1a492240ec7c0c1cf1bb6922cc4c807
palexus <palex@gmx.de>
2022-06-29 11:02:15 +0200

    clean pylint

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+"""
+	spits out the recommendations
+"""
+
 import pandas as pd
 from recommender import recommend_random
 
 
 movies = pd.read_csv("data/movies.csv")
 
-recommendations = recommend_random(None, movies=movies)
+recommendations = recommend_random(movies=movies)
 
 print(recommendations)
--- recommender.py
+++ recommender.py
@@ -1,4 +1,9 @@
-def recommend_random(query, movies, k=10):
+"""
+	Very simple recommender
+"""
+
+
+def recommend_random(movies, k=10):
     """
     Dummy recommender that recommends a list of random movies. Ignores the actual query.
     """
